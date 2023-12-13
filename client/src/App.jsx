663964c85349a465f78a15a98697949973226431
Xyd Espino <xydespino@outlook.com>
2023-12-13 11:09:21 -0500

    Progress on Log in page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import IndexPage from './pages/IndexPage';
+import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <Routes>
-        <Route index element={<IndexPage />}/>
-        <Route path="/login" element={<LoginPage/>}/>
-
-
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<IndexPage />}/>
+          <Route path="/login" element={<LoginPage/>}/>
+        </Route>
       </Routes>
   )
 }
